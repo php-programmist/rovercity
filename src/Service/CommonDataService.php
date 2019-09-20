33bdfb0e8968c7da31cc0ac3293e86aa730d0e9e
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-09-20 12:50:22 +0300

    brand name added to template

--- src/Service/CommonDataService.php
+++ src/Service/CommonDataService.php
@@ -14,18 +14,24 @@ class CommonDataService
      * @var SpecialOffersService
      */
     protected $special_offers_service;
+    /**
+     * @var BrandResolverService
+     */
+    protected $brand_resolver;
     
-    public function __construct(BrandRepository $brand_repository, SpecialOffersService $special_offers_service)
+    public function __construct(BrandRepository $brand_repository, SpecialOffersService $special_offers_service,BrandResolverService $brand_resolver)
     {
         $this->brand_repository = $brand_repository;
         $this->special_offers_service = $special_offers_service;
+        $this->brand_resolver = $brand_resolver;
     }
     
     public function getCommonData($token)
     {
         $brands = $this->brand_repository->findAll();
+        $brand = $this->brand_resolver->getBrand($token);
         $special_offers = $this->special_offers_service->getSpecialOffers($token);
-        return compact('brands','special_offers');
+        return compact('brands','brand','special_offers');
     }
     
     public function addCommonData($params, $token)
