cfb43c9addc248b9959a948b1a89d587f5812005
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-09-27 19:20:52 +0300

    File formated

--- public_html/js/modal_handler.js
+++ public_html/js/modal_handler.js
@@ -1,90 +1,109 @@
 function validateNameAndPhone(name_field, phone_field) {
-	let phone_test =/\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/;
-	let name_test =/[\da-zA-Z]/;
+	let phone_test = /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/;
+	let name_test = /[\da-zA-Z]/;
 	
 	if (!name_field.val() || name_test.test(name_field.val())) {
 		name_field.addClass('red_border');
-		 console.log("Ошибка в имени 1");
+		console.log("Ошибка в имени 1");
 		return false;
 	}
-	else{
+	else {
 		name_field.removeClass('red_border');
-		 // console.log("Имя верное");
+		// console.log("Имя верное");
 	}
 	if (!phone_test.test(phone_field.val())) {
 		phone_field.addClass('red_border');
-		 console.log("Ошибка в телефоне 1");
+		console.log("Ошибка в телефоне 1");
 		return false;
 	}
-	else{
+	else {
 		phone_field.removeClass('red_border');
-		 // console.log("Телефон верный");
+		// console.log("Телефон верный");
 	}
 	
 	////////////////////////////////////
-	var sev2=document.getElementById("sev").innerHTML;
-	
- 
-function id_ploshadki2(sev) {
+	var sev2 = document.getElementById("sev").innerHTML;
 	
 	
-				sev=sev.replace("<div>","");
-				sev=sev.replace("</div>","");
-				sev=sev.replace("<span>","");
-				sev=sev.replace("</span>","");
-				sev=sev.replace('<span style="color: rgb(0, 21, 50); text-shadow: none;">',"");
-				sev=sev.trim();
-				
-				console.log ('***'+sev+'***');
-									
-         if(sev=='+7 (495) 150-70-69') {
-						//alert('nizegor2');
-						var id_ploshadki='223429';
-   				 }
-				 else if(sev=='+7 (499) 288-24-67') {
-						//alert('lobn2');
-						var id_ploshadki='223427';
-   				 }
-				 else if(sev=='+7 (499) 288-84-58') {
-						//alert('lobn2');
-						var id_ploshadki='223425';
-   				 }
-				 else if(sev=='+7 (495) 1167139') {var id_ploshadki='224793';}
-				 
-				 else if(sev=='+7 (495) 116-99-75') {var id_ploshadki='224795';}
-				  else if(sev=='+7 (495) 116-83-60') {var id_ploshadki='224797';}
-				   else if(sev=='+7 (495) 126-34-17') {var id_ploshadki='224799';}
-				    else if(sev=='+7 (495) 116-96-75') {var id_ploshadki='224801';}
-					 else if(sev=='+7 (495) 116-70-34') {var id_ploshadki='224805';}
-					  else if(sev=='+7 (495) 116-96-26') {var id_ploshadki='224809';}
-					   else if(sev=='+7 (495) 126-18-40') {var id_ploshadki='224811';}
-					   
-    return id_ploshadki;
-};
-
-			
-			
-	  if (window.ComagicWidget) {
-var t = +new Date () + 10000;
-var id_ploshadki = id_ploshadki2(sev2);
-console.log('*'+id_ploshadki+'*'+sev2+'*');
-            yaCounter33503593.reachGoal('mango_perezvon');
-			ComagicWidget.sitePhoneCall({phone:phone_test.test(phone_field.val()), group_id: id_ploshadki, delayed_call_time: t.toString()});
+	function id_ploshadki2(sev) {
+		
+		
+		sev = sev.replace("<div>", "");
+		sev = sev.replace("</div>", "");
+		sev = sev.replace("<span>", "");
+		sev = sev.replace("</span>", "");
+		sev = sev.replace('<span style="color: rgb(0, 21, 50); text-shadow: none;">', "");
+		sev = sev.trim();
+		
+		console.log('***' + sev + '***');
+		
+		if (sev == '+7 (495) 150-70-69') {
+			//alert('nizegor2');
+			var id_ploshadki = '223429';
 		}
+		else if (sev == '+7 (499) 288-24-67') {
+			//alert('lobn2');
+			var id_ploshadki = '223427';
+		}
+		else if (sev == '+7 (499) 288-84-58') {
+			//alert('lobn2');
+			var id_ploshadki = '223425';
+		}
+		else if (sev == '+7 (495) 1167139') {
+			var id_ploshadki = '224793';
+		}
+		
+		else if (sev == '+7 (495) 116-99-75') {
+			var id_ploshadki = '224795';
+		}
+		else if (sev == '+7 (495) 116-83-60') {
+			var id_ploshadki = '224797';
+		}
+		else if (sev == '+7 (495) 126-34-17') {
+			var id_ploshadki = '224799';
+		}
+		else if (sev == '+7 (495) 116-96-75') {
+			var id_ploshadki = '224801';
+		}
+		else if (sev == '+7 (495) 116-70-34') {
+			var id_ploshadki = '224805';
+		}
+		else if (sev == '+7 (495) 116-96-26') {
+			var id_ploshadki = '224809';
+		}
+		else if (sev == '+7 (495) 126-18-40') {
+			var id_ploshadki = '224811';
+		}
+		
+		return id_ploshadki;
+	};
+	
+	
+	if (window.ComagicWidget) {
+		var t = +new Date() + 10000;
+		var id_ploshadki = id_ploshadki2(sev2);
+		console.log('*' + id_ploshadki + '*' + sev2 + '*');
+		yaCounter33503593.reachGoal('mango_perezvon');
+		ComagicWidget.sitePhoneCall({
+			phone: phone_test.test(phone_field.val()),
+			group_id: id_ploshadki,
+			delayed_call_time: t.toString()
+		});
+	}
 	///////////////////////////////////
 	console.log('отправлено');
 	return false;
 	return true;
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 	$(".phone-field").mask("+7(999)999-99-99");
 	
-	setTimeout(()=>{
+	setTimeout(() => {
 		$(".baner-wraper").slideDown();
-	},40000);
+	}, 40000);
 	
-	$('.callback-form').submit( function(e)  {
+	$('.callback-form').submit(function (e) {
 		e.preventDefault();
 		let name_field = $(this).find('input[name=name]');
 		let phone_field = $(this).find('input[name=phone]');
@@ -94,22 +113,22 @@ $(document).ready(function() {
 			return false;
 		}
 		$.ajax({
-			url:'/mail/callback/',
-			type:'POST',
-			data:{name:name_field.val(),phone:phone_field.val(),subject:subject},
+			url: '/mail/callback/',
+			type: 'POST',
+			data: {name: name_field.val(), phone: phone_field.val(), subject: subject},
 			dataType: 'json',
-			success:(data)=>{
+			success: (data) => {
 				if (data.status) {
 					alert(data.msg);
 					$('.modal-close-action').click();
 					// name_field.val('');
 					// phone_field.val('');
 				}
-				else{
-					alert("Возникли ошибки:\n - "+data.errors.join("\n - "));
+				else {
+					alert("Возникли ошибки:\n - " + data.errors.join("\n - "));
 				}
 			},
-			error:(e)=>{
+			error: (e) => {
 				console.log(e);
 				alert("Произошла ошибка")
 			}
